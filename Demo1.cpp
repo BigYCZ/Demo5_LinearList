474c8ae79c5776827b7ebb08d8433a7971c4ec89
ycz <yczup6@163.com>
2020-11-27 22:52:28 +0800

    通过顺序表进行集合A-B的差集运算

--- Demo1.cpp
+++ Demo1.cpp
@@ -5,7 +5,7 @@
 /** 示例：利用线性表的基本运算，实现如果在线性表 A中出现的元素，在线性表 B中也出现，则将 A中的该元素删除。 */
 /** 分析：这其实是求两个表的差集，即 A-B。依次检查线性表 B中的每个元素，如果该元素在线性表 A中也出现，则在 A删除该元素 */
 #include "stdio.h"
-#include "main.cpp"
+#include "main.cpp" //引入main源文件中的增加、删除、销毁数组的方法
 
 typedef int DateType;
 
@@ -25,43 +25,43 @@ void DeleteElement(SeqList *A, SeqList B) {
 }
 
 /** 测试程序如下： */
-void DeleteElement(SeqList *A, SeqList B);
+void DeleteElement(SeqList *A, SeqList B);      //删除函数声明
 
 int main() {
     int flag;
     DateType e;
-    SeqList A, B;
-    InitList(&A);
-    InitList(&B);
+    SeqList A, B;       //声明顺序表A和B
+    InitList(&A);       //初始化顺序表A
+    InitList(&B);       //初始化顺序表B
     for (int i = 1; i <= 10; i++) {
-        if (InsertList(&A, i, i + 10) == 0) {
+        if (InsertList(&A, i, i + 10) == 0) {   //将10个数据插入到顺序表A中
             printf("插入位置不合法1\n");
             return 0;
         }
     }
-    for (int i = 1, j = 1; j <= 6; i = i + 2, j++) {
+    for (int i = 1, j = 1; j <= 6; i = i + 2, j++) {    //插入到几个数据到顺序表B中
         if (InsertList(&B, j, i * 2) == 0) {
             printf("插入位置不合法2\n");
             return 0;
         }
     }
     printf("顺序表A中的元素：\n");
-    for (int i = 0; i < A.length; ++i) {
-        flag = GetElement(A, i, &e);
+    for (int i = 0; i < A.length; ++i) {    //输出顺序表A中的每个元素
+        flag = GetElement(A, i, &e);        //返回顺序表A中的每个元素到e中
         if (flag == 1)
             printf("%4d", e);
     }
     printf("\n");
     printf("顺序表B中的元素：\n");
-    for (int i = 0; i < B.length; ++i) {
-        flag = GetElement(B, i, &e);
+    for (int i = 0; i < B.length; ++i) {     //输出顺序表B中的每个元素
+        flag = GetElement(B, i, &e);        //返回顺序表B中的每个元素到e中
         if (flag == 1)
             printf("%4d", e);
     }
     printf("\n");
     printf("将在A中出现B的元素删除后，A中的元素（即A-B）：\n");
-    DeleteElement(&A, B);
-    for (int i = 0; i < A.length; ++i) {
+    DeleteElement(&A, B);                   //将在顺序表中A中出现的B的元素删除
+    for (int i = 0; i < A.length; ++i) {    //显示输出删除后A中所有元素
         flag = GetElement(A, i, &e);
         if (flag == 1)
             printf("%4d,", e);
